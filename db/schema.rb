0acf0eb4aceca63baf5be0a4ddcad0d2a2fd0ee4
Evan Washington <enavy04@gmail.com>
2016-04-11 09:44:57 -0700

    Changed the database

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160409222517) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "sun_signs", force: :cascade do |t|
     t.string   "name"
     t.string   "month"
