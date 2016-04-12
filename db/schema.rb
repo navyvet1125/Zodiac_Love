afb7d6712d52353f7dc1234fa1999f6e690ddc9b
Evan Washington <enavy04@gmail.com>
2016-04-11 19:28:54 -0700

    Updated users/index.html.erb, users/show.html.erb, and welcome/index.html.erb.  Created a new message model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411193524) do
+ActiveRecord::Schema.define(version: 20160412022712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "receiver_id"
+    t.string   "title"
+    t.text     "message_body"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "sun_signs", force: :cascade do |t|
     t.string   "name"
