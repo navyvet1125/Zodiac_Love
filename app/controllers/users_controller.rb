229646a6476569f59b606c7e1eedae046c77c59e
Evan Washington <enavy04@gmail.com>
2016-04-12 21:10:12 -0700

    Added Bootstrap, completed user updates.  created a bootstrap navigation bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    params = request.request_parameters[:user]
+    params = request.request_parameters
   	if (!User.find_by(email: params[:email]))
       @user =User.create({
         user_name: params[:user_name],
@@ -16,18 +16,29 @@ class UsersController < ApplicationController
         first_name: params[:first_name],
         last_name: params[:last_name],
 	  		born_on: params[:born_on],
+        gender: params[:gender],
+        desired_gender: params[:desired_gender],
+        height_feet: params[:height_feet],
+        height_inches: params[:height_inches],
+        zip_code: params[:zip_code],
 	  		password: params[:password],
 	  		password_confirmation: params[:password_confirmation]
-	  		})
-	  	if @user.save
-	  		month = (@user.born_on.month+10)%12
+	  	})
+	  	# verify that the new user was saved
+      if @user.save
+        # If saved, Determine user's sun sign
+        # The zodiac starts with Aries, so we will need to convert month
+	  		sign_month = (@user.born_on.month+10)%12
 	  		day = @user.born_on.day
-	  		@user_sign = SunSign.find_by(id: month)
+        # Get the sun sign associated with the sign_month
+	  		@user_sign = SunSign.find_by(id: sign_month)
+        # if the day is greater than or equal to the start of the sign then set the sign
 	  		if (@user_sign.day.to_i <= day)
-	  			@user.sun_sign_id = SunSign.find_by(id: month).id
+	  			@user.sun_sign_id = SunSign.find_by(id: sign_month).id
 	  			@user.save
+        # If the day is less than the start date, assign the previous sun sign
 	  		else
-	  			@user.sun_sign_id = SunSign.find_by(id: month).previous.id
+	  			@user.sun_sign_id = SunSign.find_by(id: sign_month).previous.id
 	  			@user.save
 	  		end
 	      	session[:user_id] = @user.id
@@ -60,7 +71,14 @@ class UsersController < ApplicationController
         :email => params[:email],
         :first_name => params[:first_name],
         :last_name => params[:last_name],
-        :born_on => params[:born_on]
+        :born_on => params[:born_on],
+        :gender => params[:gender],
+        :desired_gender => params[:desired_gender],
+        :zip_code => params[:zip_code],
+        :height_feet => params[:height_feet],
+        :height_inches => params[:height_inches],
+        :profile_title =>params[:profile_title],
+        :profile_info =>params[:profile_info]
       )
       if @user.save
         month = (@user.born_on.month+10)%12
